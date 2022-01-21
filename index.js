01f394bcdc8cb955b959fbfab753bfb718c5ee2d
nickandola <88413519+nickandola@users.noreply.github.com>
2022-01-21 14:43:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-// Import stylesheets
-import './style.css';
-
+console.log("i am index js")
 const upload = () => {
   let progressBar = document.querySelector('.progress-bar');
-  return progressBar.setAttribute('id', 'play-animation');
+  // return progressBar.setAttribute('id', 'play-animation');
+  return progressBar.classList.toggle("play-animation")
 };
 
-const btn = document.getElementsByTagName('button');
-
-// btn.addEventListener('click', upload());
+const btnHandle = document.querySelector('.btn')
+btnHandle.addEventListener('click', upload);
